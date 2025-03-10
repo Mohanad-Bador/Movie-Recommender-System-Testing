6faf81b4bd31aaf6856364b3500583f69bb4f2dc
youssefashmawy <43090520+youssefashmawy@users.noreply.github.com>
2025-03-10 23:58:51 +0200

    Entities added
    Finished implementation of Movie and User entities

--- movierecommender/src/main/java/movierecommender/Entities/Movie.java
+++ movierecommender/src/main/java/movierecommender/Entities/Movie.java
@@ -1,5 +1,55 @@
 package movierecommender.Entities;
 
-public class Movie {
+import java.util.Set;
 
+public class Movie {
+    private String movieName;
+    private String movieID;
+    private Set<String> genres;
+
+    public Movie() {
+
+    }
+
+    public Movie(String movieName, String movieID, Set<String> genres) {
+        this.movieName = movieName;
+        this.movieID = movieID;
+        this.genres = genres;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    // Setter for movieName
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    // Getter for movieID
+    public String getMovieID() {
+        return movieID;
+    }
+
+    // Setter for movieID
+    public void setMovieID(String movieID) {
+        this.movieID = movieID;
+    }
+
+    // Getter for genres
+    public Set<String> getGenres() {
+        return genres;
+    }
+
+    // Setter for genres
+    public void setGenres(Set<String> genres) {
+        this.genres = genres;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder movie = new StringBuilder(
+                "movieID: " + movieID + "\nmovieName: " + movieName + "\ngeneres: \n" + String.join(", ", this.genres));
+        return movie.toString();
+    }
 }
--- movierecommender/src/main/java/movierecommender/Entities/User.java
+++ movierecommender/src/main/java/movierecommender/Entities/User.java
@@ -1,19 +1,54 @@
 package movierecommender.Entities;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User {
-    private String userId;
+    private String userID;
     private String username;
-    private ArrayList<Movie> favoriteMovies;
-    
-    public User(){
+    private HashMap<String, Movie> favoriteMoviesDict;
+
+    public User() {
 
     }
-    public User(String userId,String username,ArrayList<Movie> favoriteMovies){
-        this.userId = userId;
+
+    public User(String userID, String username, HashMap<String, Movie> favoriteMoviesDict) {
+        this.userID = userID;
         this.username = username;
-        this.favoriteMovies = favoriteMovies;
+        this.favoriteMoviesDict = favoriteMoviesDict;
     }
-    
+
+    public void setuserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public void SetfavoriteMoviesDict(HashMap<String, Movie> favoriteMoviesDict) {
+        this.favoriteMoviesDict = favoriteMoviesDict;
+    }
+
+    public String getuserID() {
+        return this.userID;
+    }
+
+    public String getUserName() {
+        return this.username;
+    }
+
+    public HashMap<String, Movie> getfavoriteMoviesDict() {
+        return this.favoriteMoviesDict;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder user = new StringBuilder(
+                "userID: " + userID + "\nUserName: " + username + "\nfavoriteMovies: \n");
+        this.favoriteMoviesDict.forEach((key, value) -> {
+            user.append(value.getMovieName() + ", ");
+        });
+        return user.toString();
+    }
 }
